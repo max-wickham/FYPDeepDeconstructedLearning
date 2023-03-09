203b11614ed2e9f52325de7bf903076df9379af5
max-wickham <maxwickham@startmail.com>
2023-03-09 18:39:18 +0000

    saved model

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ from src.algorithms.PPO import PPO
 
 
 if __name__ == '__main__':
+    #ppo = PPO()
+    #ppo.load('test_model')
+    #ui = GameUI(SpaceInvaders, model = ppo)
+    #ui.run()
+    print('Starting Training')
     ppo = PPO()
-    ppo.load('test_model')
-    ui = GameUI(SpaceInvaders, model = ppo)
-    ui.run()
-
-    # ppo = PPO()
-    # ppo.train(SpaceInvaders, SimpleNetwork, save_location = 'test_model')
+    print('Training PPO')
+    print('######################')
+    ppo.train(SpaceInvaders, SimpleNetwork, save_location = 'ppo_1')
