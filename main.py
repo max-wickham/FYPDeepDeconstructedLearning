904be945de87f2500b8a7bb86980eac097056751
Max Wickham <maxwickhammonro@gmail.com>
2023-03-12 16:49:41 +0000

    changes

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ from src.algorithms.PPO import PPO
 
 
 if __name__ == '__main__':
-    # ppo = PPO()
-    # ppo.load('test_model')
-    # ui = GameUI(SpaceInvaders, model = ppo)
-    # ui.run()
-    print('Starting Training')
     ppo = PPO()
-    print('Training PPO')
-    print('######################')
-    ppo.train(SpaceInvaders, SimpleNetwork, save_location = f'{os.environ["PBS_O_WORKDIR"]}/ppo_2')
-    ppo.train(SpaceInvaders, SimpleNetwork, save_location = 'ppo_1')
+    ppo.load('ppo_2')
+    ui = GameUI(SpaceInvaders, model = ppo)
+    ui.run()
+    # print('Starting Training')
+    # ppo = PPO()
+    # print('Training PPO')
+    # print('######################')
+    # ppo.train(SpaceInvaders, SimpleNetwork, save_location = f'{os.environ["PBS_O_WORKDIR"]}/ppo_2')
+    # ppo.train(SpaceInvaders, SimpleNetwork, save_location = 'ppo_1')
