7d42a24355dcae12698d6f55174dc544a8267f9d
Max Wickham <maxwickhammonro@gmail.com>
2023-04-13 20:32:26 +0100

    fixed critic loss

--- src/algorithms/PPO.py
+++ src/algorithms/PPO.py
@@ -2,6 +2,11 @@
 PPO Algorithm
 Algorithm Pseudo Code https://spinningup.openai.com/en/latest/algorithms/ppo.html
 '''
+from src.interfaces.game import Game
+from src.interfaces.network import Network
+import tensorflow_probability as tfp
+from tensor_annotations import axes
+import tensor_annotations.tensorflow as ttf
 import dataclasses
 from dataclasses import field
 import multiprocessing
@@ -18,19 +23,11 @@ import numpy as np
 import numpy.typing as npt
 # Importing tensorflow modules this way makes type hinting work because reasons
 import tensorflow as tf
-keras = tf.keras;
+keras = tf.keras
 Adam = tf.keras.optimizers.Adam
 kls = tf.keras.losses
 # from tensorflow.keras.optimizers import Adam
-import tensor_annotations.tensorflow as ttf
-from tensor_annotations import axes
 # import tensorflow.keras.losses as kls
-import tensorflow_probability as tfp
-
-from src.interfaces.network import Network
-from src.interfaces.game import Game
-
-
 
 
 @dataclasses.dataclass
@@ -45,17 +42,18 @@ class Trajectories:
     def get_observations(self):
         return self.observations
 
+
 def create_trajectories_process(
-    num_observations : int,
-    input_queue : multiprocessing.Queue,
-    output_queue : multiprocessing.Queue,
-    stop_queue : multiprocessing.Queue,
-    game_type : Type[Game],
-    observation_dims : int,
-    action_dims : int,
-    network_type : type[Network],
-    load_location : str
-    ):
+    num_observations: int,
+    input_queue: multiprocessing.Queue,
+    output_queue: multiprocessing.Queue,
+    stop_queue: multiprocessing.Queue,
+    game_type: Type[Game],
+    observation_dims: int,
+    action_dims: int,
+    network_type: type[Network],
+    load_location: str
+):
 
     while True:
         if not stop_queue.empty():
@@ -74,7 +72,8 @@ def create_trajectories_process(
         rewards = []
         probs = []
         discount_cumulative_rewards = []
-        network_controller = PPO.NetworkController(network_type, observation_dims, action_dims, load = True)
+        network_controller = PPO.NetworkController(
+            network_type,network_type, observation_dims, action_dims, load=True)
         network_controller.load(load_location)
         while observation_count < num_observations:
             game = game_type()
@@ -88,11 +87,13 @@ def create_trajectories_process(
             action_repetitions = 0
             while not done and observation_count < num_observations:
                 observation = game.get_model_input()
-                observation = tf.reshape(observation,shape=(1,len(observation)))
+                observation = tf.reshape(
+                    observation, shape=(1, len(observation)))
                 if action_repetitions == 0:
                     observation_count += 1
                     # TODO create network controller from model file
-                    prob, action = network_controller.get_prob_action(observation)
+                    prob, action = network_controller.get_prob_action(
+                        observation)
                     prev_prob = prob
                     prev_action = action
                 done, reward = game.step(
@@ -130,6 +131,7 @@ def create_trajectories_process(
 
         continue
 
+
 def action_to_action_array(action: int) -> list[int]:
     '''Convert the combination action index to a action array'''
     # print(action.numpy()[0])
@@ -144,31 +146,32 @@ class PPO:
 
     class NetworkController:
         '''Actor and critic models'''
-        actor : Network
-        critic : Network
+        actor: Network
+        critic: Network
 
-        def __init__(self, network: type[Network], input_dims: int, output_dims: int, load = False) -> None:
+        def __init__(self, actor_network: type[Network], critic_network:  type[Network], input_dims: int, output_dims: int, load=False) -> None:
             if load:
                 return
             self.learning_rate = 0.005
 
             # takes an observation and outputs a set of actions
             # square action dims to get output space
-            self.actor = network((input_dims), output_dims * output_dims)
+            self.actor = actor_network((input_dims), output_dims * output_dims)
             # takes an observation and outputs a value
-            self.critic = network((input_dims), 1)
+            self.critic = critic_network((input_dims), 1)
             self.actor_optimiser = Adam(lr=self.learning_rate)
             self.critic_optimiser = Adam(lr=self.learning_rate)
 
-        def load(self, load_location : str):
+        def load(self, load_location: str, load_critic = False):
             print('Load')
             self.actor = keras.models.load_model(
-                f'{load_location}/actor',  custom_objects={'tf': tf}) # type: ignore
+                f'{load_location}/actor',  custom_objects={'tf': tf})  # type: ignore
             print('Actor Loaded')
-            self.critic = keras.models.load_model(
-                f'{load_location}/critic',  custom_objects={'tf': tf}) # type: ignore
+            if load_critic:
+                self.critic = keras.models.load_model(
+                    f'{load_location}/critic',  custom_objects={'tf': tf})  # type: ignore
 
-        def save(self, save_location : str):
+        def save(self, save_location: str):
             if not os.path.exists(f'{save_location}'):
                 os.mkdir(f'{save_location}')
             self.actor.model.save(f'{save_location}/actor')
@@ -196,31 +199,36 @@ class PPO:
     class Trainer:
         '''Train a PPO model'''
 
-        def __init__(self, game_type: type[Game], network: type[Network], save_location : str) -> None:
+        def __init__(self,
+                     game_type: type[Game],
+                     actor_network: type[Network],
+                     critic_network: type[Network],
+                     save_location: str) -> None:
             self.save_location = save_location
             self.action_dims = game_type.get_action_shape()
             self.observation_dims = game_type.get_input_shape()
+            self.actor_network_type = actor_network
+            self.critic_network_type = critic_network
             # self.total_time_steps = 10000000
-            self.total_time_steps = 60000000
-            self.observations_per_batch = 20000
-            self.num_workers = 10
-            # self.total_time_steps = 10000
-            # self.observations_per_batch = 500
+            # self.total_time_steps = 60000000
+            # self.observations_per_batch = 20000
+            self.num_workers = 1
+            self.total_time_steps = 1000
+            self.observations_per_batch = 500
 
             self.updates_per_iteration = 10
             self.game_type = game_type
-            self.network_type = network
             self.gamma = 0.95
             self.clip = 0.2
             self.repeat_action_num = 3
 
             self.network_controller = PPO.NetworkController(
-                network, self.observation_dims, self.action_dims)
+                actor_network, critic_network, self.observation_dims, self.action_dims)
 
             self.task_queue = multiprocessing.Queue()
             self.response_queue = multiprocessing.Queue()
             self.stop_queue = multiprocessing.Queue()
-            self.workers : list[multiprocessing.Process]  = []
+            self.workers: list[multiprocessing.Process] = []
 
         def create_trajectories_parralel(self) -> Trajectories:
 
@@ -229,7 +237,7 @@ class PPO:
                 self.task_queue.put(True)
 
             num_responses = 0
-            trajectories_list : list[Trajectories] = []
+            trajectories_list: list[Trajectories] = []
             while len(trajectories_list) < len(self.workers):
                 if not self.response_queue.empty():
                     print('Received Response')
@@ -238,13 +246,17 @@ class PPO:
 
             print('Concatinating Results')
             trajectories = Trajectories(
-                np.concatenate(tuple(trajectories.observations for trajectories in trajectories_list)),
-                np.array([action for trajectories in trajectories_list for action in list(trajectories.actions)]),
-                np.array([reward for trajectories in trajectories_list for reward in list(trajectories.rewards)]),
+                np.concatenate(
+                    tuple(trajectories.observations for trajectories in trajectories_list)),
+                np.array([action for trajectories in trajectories_list for action in list(
+                    trajectories.actions)]),
+                np.array([reward for trajectories in trajectories_list for reward in list(
+                    trajectories.rewards)]),
                 np.concatenate(tuple(np.reshape(trajectories.probabilities,
-                (trajectories.probabilities.shape[0],1,trajectories.probabilities.shape[1]))
-                for trajectories in trajectories_list)),
-                np.array([reward for trajectories in trajectories_list for reward in list(trajectories.discount_cumulative_rewards)]),
+                                                (trajectories.probabilities.shape[0], 1, trajectories.probabilities.shape[1]))
+                                     for trajectories in trajectories_list)),
+                np.array([reward for trajectories in trajectories_list for reward in list(
+                    trajectories.discount_cumulative_rewards)]),
             )
             # print(trajectories)
             return trajectories
@@ -275,7 +287,8 @@ class PPO:
                     observation = game.get_model_input()
                     if action_repetitions == 0:
                         observation_count += 1
-                        prob, action = self.network_controller.get_prob_action(observation)
+                        prob, action = self.network_controller.get_prob_action(
+                            observation)
                         prev_prob = prob
                         prev_action = action
                     done, reward = game.step(
@@ -310,7 +323,7 @@ class PPO:
             )
 
         def compute_value_advantage_estimates(self, trajectories: Trajectories
-            ) -> tuple[npt.NDArray, npt.NDArray]:
+                                              ) -> tuple[npt.NDArray, npt.NDArray]:
             '''For a given set of trajectories calculate the set of advantage estimates'''
             # compute the value function for the set of trajectories using the critic
             values: np.ndarray = self.network_controller.get_values(
@@ -331,18 +344,23 @@ class PPO:
             '''Calculate the actor loss'''
             entropy = tf.reduce_mean(tf.math.negative(
                 tf.math.multiply(new_probs, tf.math.log(new_probs))))
+            print(new_probs)
+            print(entropy)
             surrogate_1 = []
             surrogate_2 = []
 
-            advantages_tensor = tf.convert_to_tensor(advantages, dtype=tf.float32)
+            advantages_tensor = tf.convert_to_tensor(
+                advantages, dtype=tf.float32)
+            print(advantages_tensor)
             # new_probs_indexed = tf.convert_to_tensor(
             #     np.array([prob[action] for prob, action in zip(new_probs, actions)]), dtype=tf.float32)
-            current_probs = np.reshape(current_probs, (current_probs.shape[0], current_probs.shape[2]))
+            current_probs = np.reshape(
+                current_probs, (current_probs.shape[0], current_probs.shape[2]))
             current_probs_indexed = tf.convert_to_tensor(
                 np.array([prob[action] for prob, action in zip(current_probs, actions)]), dtype=tf.float32)
             new_probs_indexed = tf.gather_nd(
                 new_probs,
-                indices = tf.constant([[index, action] for index, action in enumerate(actions)]))
+                indices=tf.constant([[index, action] for index, action in enumerate(actions)]))
             # current_probs_indexed = tf.gather_nd(
             #     current_probs,
             #     indices = tf.constant([[index, action] for index, action in enumerate(actions)]))
@@ -367,10 +385,13 @@ class PPO:
                 advantage_estimates, (len(advantage_estimates),))
             current_probs = tf.reshape(trajectories.probabilities,
                                        (len(trajectories.probabilities), self.action_dims*self.action_dims))
+            print()
 
             with tf.GradientTape() as critic_tape, tf.GradientTape() as actor_tape:
-                actor_tape.watch(self.network_controller.actor.model.trainable_variables)
-                critic_tape.watch(self.network_controller.critic.model.trainable_variables)
+                actor_tape.watch(
+                    self.network_controller.actor.model.trainable_variables)
+                critic_tape.watch(
+                    self.network_controller.critic.model.trainable_variables)
                 probabilities = self.network_controller.actor(
                     trajectories.observations, training=True, multi_dim=True)
                 # actor_tape.watch(self.network_controller.actor.model.trainable_variables)
@@ -379,6 +400,7 @@ class PPO:
                 values = tf.reshape(values, (len(values),))
                 critic_loss = 0.5 * \
                     kls.mean_squared_error(discount_cumulative_rewards, values)
+                print('Computing Loss')
                 actor_loss = self.actor_loss(
                     probabilities, trajectories.probabilities, trajectories.actions, advantage_estimates, critic_loss)
 
@@ -398,9 +420,9 @@ class PPO:
                 os.mkdir(f'{self.save_location}')
             configs = {
                 'game_type': str(type(self.game_type)),
-                'network_type': str(type(self.network_type))
+                'network_type': str(type(self.actor_network_type))
             }
-            with open(f'{self.save_location}/configs.json','w', encoding='utf8') as file:
+            with open(f'{self.save_location}/configs.json', 'w', encoding='utf8') as file:
                 file.write(json.dumps(configs))
             self.network_controller.save(self.save_location)
             # self.network_controller.actor.model.save(f'{self.save_location}/actor')
@@ -411,7 +433,6 @@ class PPO:
             BaseManager.register('Trajectories', Trajectories)
             manager = BaseManager()
             manager.start()
-
             self.workers = []
             for _ in range(self.num_workers):
                 process = Process(target=create_trajectories_process, args=[
@@ -422,14 +443,13 @@ class PPO:
                     self.game_type,
                     self.observation_dims,
                     self.action_dims,
-                    self.network_type,
+                    self.actor_network_type,
                     self.save_location])
                 self.workers.append(process)
                 process.start()
 
         def stop_workers(self):
             self.stop_queue.put(True)
-
 
         def train(self):
             '''Train the model by running games'''
@@ -438,51 +458,55 @@ class PPO:
             batches = 0
             print('training')
             self.create_workers()
-            try:
-                while time_steps < self.total_time_steps:
-                    print('Steps',time_steps)
-                    batches += 1
-                    # trajectories = self.create_trajectories()
-                    trajectories = self.create_trajectories_parralel()
-                    print('Created Trajectories')
-                    time_steps += len(trajectories.observations)
+            while time_steps < self.total_time_steps:
+                print('Steps', time_steps)
+                batches += 1
+                # trajectories = self.create_trajectories()
+                trajectories = self.create_trajectories_parralel()
+                print('Created Trajectories')
+                time_steps += len(trajectories.observations)
 
-                    values, advantages = self.compute_value_advantage_estimates(
-                        trajectories)
+                values, advantages = self.compute_value_advantage_estimates(
+                    trajectories)
+            print('Collected Data')
+            for _ in range(self.updates_per_iteration):
+                print('Updating')
+                actor_loss, critic_loss = self.update_policy(
+                    trajectories, advantages)
 
-                for _ in range(self.updates_per_iteration):
-                    actor_loss, critic_loss = self.update_policy(
-                        trajectories, advantages)
+                self.save()
 
-                    self.save()
-            except Exception:
-                ...
             self.stop_workers()
 
     def load(self, load_location: str):
         '''Load a pre-trained model'''
         self.actor = keras.models.load_model(
-            f'{load_location}/actor',  custom_objects={'tf': tf}) # type: ignore
+            f'{load_location}/actor',  custom_objects={'tf': tf})  # type: ignore
 
     def compute_action(self, game: Game):
         '''Compute the actions of a current game state of the loaded model'''
         input_vector = game.get_model_input()
         input_vector = tf.convert_to_tensor(
-                np.array(input_vector).reshape(-1).reshape(
-                    1, len(input_vector)
-                ))
-        prob : ttf.Tensor1 = self.actor(input_vector) # type: ignore
+            np.array(input_vector).reshape(-1).reshape(
+                1, len(input_vector)
+            ))
+        prob: ttf.Tensor1 = self.actor(input_vector)  # type: ignore
         prob = prob.numpy()[0]
         dist = tfp.distributions.Categorical(probs=prob, dtype=tf.float32)
         action = dist.sample(1)
         return action_to_action_array(action)
 
-    def train(self, game: type[Game], network: type[Network], save_location='model'):
+    def train(self,
+              game: type[Game],
+              actor_network: type[Network],
+              critic_network: type[Network],
+              save_location='model'):
         '''Train the model on a specific game and network'''
-        trainer = PPO.Trainer(game, network, save_location)
+        trainer = PPO.Trainer(game, actor_network,
+                              critic_network, save_location)
         trainer.train()
 
-    actor : keras.Model
+    actor: keras.Model
 
     def __init__(self) -> None:
         ...
